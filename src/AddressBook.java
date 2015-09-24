6f84d97832d311846397625fcab93dcd2c31c153
Monty1029 <Monty@KAMI>
2015-09-24 12:13:37 -0400

    commit

---
+++ src/AddressBook.java
@@ -0,0 +1,31 @@
+import java.util.*;
+//comment
+public class AddressBook {
+
+	private List<BuddyInfo> buddyInfo;
+
+	public AddressBook() {
+		this.buddyInfo = new ArrayList<BuddyInfo>();
+	}
+
+	public void addBuddy(BuddyInfo buddy) {
+		if (buddy != null) {
+			this.buddyInfo.add(buddy);
+		}
+	}
+
+	public BuddyInfo removeBuddy(int index) {
+		if (index >= 0 && index < this.buddyInfo.size()) {
+			return this.buddyInfo.remove(index);
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
+		BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", 1234);
+		AddressBook address_book = new AddressBook();
+		address_book.addBuddy(buddy);
+		address_book.removeBuddy(0);
+
+	}
+}
